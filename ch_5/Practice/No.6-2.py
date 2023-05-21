11bf029a63c1f620b2291853ba01591e464369b3
nowuri <be2110421@sookmyung.ac.kr>
2023-05-21 11:18:44 +0900

    2023_05_21

--- ch_5/Practice/No.6-2.py
+++ ch_5/Practice/No.6-2.py
@@ -6,27 +6,28 @@ gray = cv.cvtColor(img,cv.COLOR_BGR2GRAY)
 
 height, width = gray.shape
 
+n = int(input('가우시안 영상의 개수(최대 6): '))
+
 gaussian = []
-gaussian.append(cv.GaussianBlur(gray, (5,5),1.6))
-gaussian.append( cv.GaussianBlur(gray,(5,5),2.0159))
-gaussian.append( cv.GaussianBlur(gray, (5,5), 2.5398))
-gaussian.append(cv.GaussianBlur(gray,(5,5),3.2))
-gaussian.append(cv.GaussianBlur(gray,(5,5),4.0317))
-gaussian.append(cv.GaussianBlur(gray,(5,5), 5.0797))
+num = [1.6, 2.0159, 2.5398, 3.2, 4.0317, 5.0797]
+
+for i in range(n):
+    print(num[i])
+    gaussian.append(cv.GaussianBlur(gray, (5,5), num[i]))
 
 DoG =[]
-for i in range(5):
+for i in range(n):
     DoG.append(np.zeros_like(gray))
 
-for k in range(4):
+for k in range(n-1):
     for i in range(height):
         for j in range(width):
             DoG[k][i][j] = float(gaussian[k+1][i][j]) - float(gaussian[k][i][j])
 
-for i in range(5):
+for i in range(n-1):
     pic = DoG[i]
     pic = cv.resize(pic, dsize=(0,0), fx=0.4, fy=0.4)
-    cv.imshow('DoG'+str(i), pic);
+    cv.imshow('DoG'+str(i), pic)
 
 cv.waitKey()
 cv.destroyAllWindows()
